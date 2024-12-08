fa2198972d8238f3fa0d371fb16b6ea4d90543b9
saurav-tiwari03 <sauravtiwari5262@gmail.com>
2024-12-08 15:44:45 +0530

    todo-list

--- array/main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func main() {
-	var fruitList = [4]string{"Apple", "Banana", "Orange", "Mango"}
-	fmt.Println("Fruit list:", fruitList)
-	fmt.Printf("Length: %d, Type: %T\n", len(fruitList), fruitList)
-}
